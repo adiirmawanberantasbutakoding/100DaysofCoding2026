f0d7d036ed2a7fd085bd26af94e7470bae2613be
adiirmawanberantasbutakoding <adiirmawanj.smkrangas@gmail.com>
2026-09-02 21:38:41 +0800

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -1,19 +1,20 @@
-// Program Day 1 - 100 Hari Ngoding Java
 // Nama: Adi Irmawan
+// // = komentar 1 baris, buat catatan singkat
 
 /**
- * Kelas tugas day 1
+ * /** ... */ = javadoc, buat dokumentasi class
  */
 public class Day1 {
     public static void main(String[] args) { 
         
-        // menampilkan teks ke layar
+        // // = komentar 1 baris, buat jelasin 1 baris kode di bawah
         System.out.println("hari 1 ngoding");
-        
+
         /*
+        /* ... */ = komentar banyak baris, buat jelasin yang panjang
         tugas day 1:
         1. belajar println
-        2. belajar komentar
+        2. belajar 3 jenis komentar
         */
     }
 }
