517cec3defd78f643fd84b481326af1823b50756
adiirmawanberantasbutakoding <adiirmawanj.smkrangas@gmail.com>
2026-09-04 15:23:33 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,18 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		//  \n fungsi turun ke baris baru
+		System.out.println("baris 1 \nbaris 2");
+		
+		// \t  fungsi spasi agak jauh
+		System.out.println("lumayan \tjauh");
+		
+	// \b fungsi buat hapus 1 huruf di belakang
+		System.out.println("BCD\bEF");
+		
+		// \"  fungsi biar bisa di pake " didalam "..."
+		System.out.println("jangan lupa \"subscriber\" ");
+		
+		
+	}
+}
