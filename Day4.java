e75786feb1858cf6de7a32375ac90dd75c083396
adiirmawanberantasbutakoding <adiirmawanj.smkrangas@gmail.com>
2026-09-05 14:31:26 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,26 @@
+public class main{
+    public static void main(String[] args) {
+        
+        // BYTE: buat data kecil. Contoh: umur, nilai 0-100
+        byte umur = 74 ; 
+        System.out.println("umur:" + umur  +  "tahun");
+        
+        
+        // SHORT: buat data sedang. Contoh: tahun
+        short tahun = 1998;
+        System.out.println("tragedi:" + tahun );
+        
+        
+        // INT: ANDALAN. Default buat angka bulat
+        int jumlahsiswa = 10000;
+        System.out.println("jumlah siswa:" + jumlahsiswa);
+       
+        
+        // LONG: buat angka gede. WAJIB ada L di belakang
+      long tabungansaya  = 100000000000L;
+      System.out.println("tabungan saya:" + tabungansaya);
+        
+        
+        
+    }
+}
