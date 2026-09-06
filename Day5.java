af4525647ce86b76d06b14e0d996335182472b33
adiirmawanberantasbutakoding <adiirmawanj.smkrangas@gmail.com>
2026-09-06 10:06:09 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,23 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		// float harus ada f di belakang
+		
+		float tinggibadan  = 168.5f;
+		float beratbadan = 55.2f;
+        
+        System.out.println("tinggi:" + tinggibadan + "cm");
+        System.out.println("berat:" + beratbadan +"kg");
+		
+						
+		// tipe data double buat menyimpan bilangan
+		
+		double nilairupiah = 17630.80;
+		double hargabarang = 380000.00;
+		System.out.println("nilai rupiah:" + nilairupiah );
+		System.out.println("harga barang: " + hargabarang);
+		
+		
+		
+	}
+}
