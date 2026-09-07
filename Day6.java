f0297f42d05a38526b65c29cccbd5ffb4d20da91
adiirmawanberantasbutakoding <adiirmawanj.smkrangas@gmail.com>
2026-09-07 20:47:05 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,29 @@
+public class Main {
+	public static void main(String[] args) {
+		 
+		  // tipe data char buat nyimpan 1 data karakter huruf,angka dan  simbol
+		  
+		  char jenisKelamin = 'L';
+		  char nilai = '9';
+		  char simbol = '€';
+		  System.out.println ("jenis kelamin:" + jenisKelamin);
+		  System.out.println("nilai :" + nilai);
+		  System.out.println("simbol:" + simbol);
+		  
+		  // boolean tipe data buat nyimpan kondisi benar atau salah
+		  
+		  int nilaiDdp = 99;
+System.out.println("nilai Ddp:" + nilaiDdp);
+
+boolean lulusDdp = nilaiDdp >= 98; 
+
+if (lulusDdp) { 
+    System.out.println("status:" + lulusDdp);
+    System.out.println("selamat kamu lulus Ddp");
+} else { 
+    System.out.println("status:" + lulusDdp);
+    System.out.println("selamat anda tidak lulus Ddp");
+		 		  		  		      		      					  
+      }
+	}
+}
