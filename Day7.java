3c65b8c7666d97f309ef3adad670c150a8500ef8
adiirmawanberantasbutakoding <adiirmawanj.smkrangas@gmail.com>
2026-09-08 16:04:02 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,21 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		// tipe data string adalah tipe data buat nyimpan teks
+		
+		// fungsi string menyimpan data berupa kalimat,nama,angka dan simbol
+		
+		String halo = "indonesia";
+		String nama_saya = "adi";
+		String umur = "19";
+		String mata_uang = "$";
+		
+		System.out.println("halo:" + halo);
+		System.out.printf("nama saya %s dan umur saya %s tahun \n", nama_saya,umur);
+		System.out.println("mata uang:" + mata_uang);
+		
+		
+		
+		
+	}
+}
