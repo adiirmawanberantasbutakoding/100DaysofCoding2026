1264e792aa01b9b42a0cc239ededc9cffa198d02
adiirmawanberantasbutakoding <adiirmawanj.smkrangas@gmail.com>
2026-09-09 12:53:30 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+    
+    // deklarasi    
+    String masaJabatan;
+    
+    // inisialisasi 
+     masaJabatan = ("jokowidodo dan ma'ruf amin masa periode 2019 2024");
+    
+    System.out.println(masaJabatan);
+    
+    // update
+    masaJabatan = ("prabowo subianto dan gibran rakabuming raka masa periode 2024 2030");
+    System.out.println(masaJabatan);
+    
+  
+    
+    }
+}
