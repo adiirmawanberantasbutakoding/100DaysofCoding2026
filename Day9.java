debf3f6561948e3c1d109acb30ae6a9d09748ca7
adiirmawanberantasbutakoding <adiirmawanj.smkrangas@gmail.com>
2026-09-10 11:26:01 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		// konstanta
+	
+		final String NAMA = " nama: adi";
+		final String STATUS =" status: WNI seumur hidup";
+
+		System.out.println(NAMA);
+		System.out.println(STATUS);
+		
+	}
+}
