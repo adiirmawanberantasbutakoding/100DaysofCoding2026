c2805ed0b562998e81f2ffab42cce583d72a9af5
adiirmawanberantasbutakoding <adiirmawanj.smkrangas@gmail.com>
2026-09-11 14:40:11 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("TIPE DATA INTEGER ");
+        System.out.println("1. byte   : " + Byte.MIN_VALUE + " sampai " + Byte.MAX_VALUE);
+        System.out.println("2. short  : " + Short.MIN_VALUE + "sampai " + Short. MAX_VALUE);
+        System.out.println("3. int    : " + Integer.MIN_VALUE + " sampai " + Integer.MAX_VALUE);
+        System.out.println("4. long   : " + Long.MIN_VALUE + " sampai " + Long.MAX_VALUE);
+
+        System.out.println("\n TIPE DATA KARAKTER ");
+        System.out.println("5. char   : " + (int)Character.MIN_VALUE + " sampai " + (int)Character.MAX_VALUE);
+        
+        System.out.println("\n TIPE DATA FLOAT ");
+        System.out.println("6. float  : " + Float.MIN_VALUE + " sampai " + Float.MAX_VALUE);
+        System.out.println(" \n 7. double : " + Double.MIN_VALUE + " sampai " + Double.MAX_VALUE);
+
+        System.out.println("TIPE DATA BOOLEAN");
+        System.out.println("8. boolean: true atau false");
+    }
+}
