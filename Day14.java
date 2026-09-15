dcf0c5f1590cd8031b57bf0dda526b3751dda84a
adiirmawanberantasbutakoding <adiirmawanj.smkrangas@gmail.com>
2026-09-15 21:43:20 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,9 @@
+public class Operator {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 10;
+
+        System.out.println(a + b); 
+        System.out.println(a - b);
+    }
+}
