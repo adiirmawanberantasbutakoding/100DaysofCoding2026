213ef33c3c2d60e94f01701e0abfaefc66aafa68
adiirmawanberantasbutakoding <adiirmawanj.smkrangas@gmail.com>
2026-09-16 21:53:02 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 5;
+
+        System.out.println(a * b); 
+        System.out.println(a / b);
+    }
+}
