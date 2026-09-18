0071dd9a8a30263269dfe51adb22b95ffca3926b
adiirmawanberantasbutakoding <adiirmawanj.smkrangas@gmail.com>
2026-09-18 20:43:47 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,20 @@
+public class day17{
+    public static void main(String[]args){
+        int total = 20;
+        total += 12;
+        int stok = 25;
+        stok -= 7;
+        int harga = 15;
+        harga *= 4;
+        int saldo = 50;
+        saldo /= 5;
+        int sisa = 25;
+        sisa %= 6;
+
+        System.out.println("total:"+total);
+        System.out.println("stok:"+stok);
+        System.out.println("harga:"+harga);
+        System.out.println("saldo:"+saldo);
+        System.out.println("sisa:"+sisa);
+    }
+}
