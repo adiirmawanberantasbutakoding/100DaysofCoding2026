0c282695068907060fa8509eb9a844a5e0acd3ac
adiirmawanberantasbutakoding <adiirmawanj.smkrangas@gmail.com>
2026-09-19 20:27:45 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class KonversiOtomatis {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Input nilai byte: ");
+        byte dataByte = input.nextByte();
+
+        
+        short dataShort = dataByte;
+        int dataInt = dataByte;
+        long dataLong = dataInt;
+        float dataFloat = dataLong;
+        double dataDouble = dataFloat;
+
+        System.out.println("\n--- Hasil Widening ---");
+        System.out.println("Dari byte " + dataByte + " menjadi:");
+        System.out.println("short = " + dataShort);
+        System.out.println("int = " + dataInt);
+        System.out.println("long = " + dataLong);
+        System.out.println("float = " + dataFloat);
+        System.out.println("double = " + dataDouble);
+
+        input.close();
+    }
+  
