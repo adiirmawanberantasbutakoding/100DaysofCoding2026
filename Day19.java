cd0455e732f4be5ca3fc686d0a984290cf7a3853
adiirmawanberantasbutakoding <adiirmawanj.smkrangas@gmail.com>
2026-09-20 21:54:20 +0800

    Create Day19.java

---
+++ Day19.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        double nilaiTranskrip = 90.40; 
+        int nilaiBulat = (int) nilaiTranskrip;
+
+        
+        int nilaiUjian = 200; 
+        byte nilaiKecil = (byte) nilaiUjian;  
+        
+
+        System.out.println(nilaiBulat);
+        System.out.println(nilaiKecil);
+    }
+}
