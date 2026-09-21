4b1987d61718cd5d290c0b51a843456069418254
adiirmawanberantasbutakoding <adiirmawanj.smkrangas@gmail.com>
2026-09-21 21:42:03 +0800

    Create Day20.java

---
+++ Day20.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukin int: ");
+        int umur = input.nextInt();
+        
+        System.out.print("Masukin double: ");
+        double nilai = input.nextDouble();
+        
+        System.out.print("Masukin boolean (true/false): ");
+        boolean lulus = input.nextBoolean();
+
+        
+        String sInt = String.valueOf(umur);
+        String sDouble = String.valueOf(nilai);
+        String sBoolean = String.valueOf(lulus);
+        
+        
+        char grade = 'A';
+        String sChar = String.valueOf(grade);
+        long saldo = 1000000L;
+        String sLong = String.valueOf(saldo);
+
+        System.out.println("\n--- HASIL STRING ---");
+        System.out.println("int -> String: " + sInt + " ( " + sInt + 10 + " )");
+        System.out.println("double -> String: " + sDouble);
+        System.out.println("boolean -> String: " + sBoolean);
+        System.out.println("char -> String: " + sChar);
+        System.out.println("long -> String: " + sLong);
+        
+        input.close();
+    }
+}
