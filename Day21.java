f5ed51a257ce2a89be4459484b401412bda48d20
adiirmawanberantasbutakoding <adiirmawanj.smkrangas@gmail.com>
2026-09-22 22:08:36 +0800

    Create Day21.java

---
+++ Day21.java
@@ -0,0 +1,13 @@
+public class DayMain{
+    public static void main(String[] args) {
+        String s = "123";
+
+        int a = Integer.parseInt(s);
+        double b = Double.parseDouble("3.14");
+        boolean c = Boolean.parseBoolean("true");
+
+        System.out.println(a + 10); 
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
